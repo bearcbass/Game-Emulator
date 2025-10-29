a50b5aa12de06e071cd1015f159c4503595bb003
Sebastian Osorio <bearcbass@gmail.com>
2025-10-29 13:28:17 -0400

    Added more features to the CPU component

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -10,13 +10,53 @@ pub struct CPU {
     l: u8,
     
     // 16-bit registers
-    pc: u16, // Stack Pointer
-    sp: ua6, // Program Counter
+    pc: u16, // Program Counter
+    sp: u16, // Stack Counter
 
 }
 
 impl CPU {
     pub fn new() -> Self {
-        CPU {}
+        CPU {
+            a: 0,
+            f: 0,
+            b: 0,
+            c: 0,
+            d: 0,
+            e: 0,
+            h: 0,
+            l: 0,
+            sp: 0,
+            pc: 0,
+        }
+    }
+
+    // Add all methods here
+
+    pub fn get_bc(&self) -> u16 {
+        self.b as u16 << 8 | self.c as u16
+    }
+
+    pub fn set_bc(&mut self, value: u16) {
+        self.b = (value >> 8) as u8;
+        self.c = (value & 0xFF) as u8;
+    }
+
+    pub fn get_de(&self) -> u16 {
+        (self.d as u16 << 8) | (self.e as u16)
+    }
+
+    pub fn set_de(&mut self, value: u16) {
+        self.d = (value >> 8) as u8;
+        self.e = (value & 0xFF) as u8;
+    }
+
+    pub fn get_hl(&self) -> u16 {
+        (self.h as u16 << 8) | (self.l as u16)
+    }
+
+    pub fn set_hl(&mut self, value: u16) {
+        self.h = (value >> 8) as u8;
+        self.l = (value & 0xFF) as u8;
     }
 }
